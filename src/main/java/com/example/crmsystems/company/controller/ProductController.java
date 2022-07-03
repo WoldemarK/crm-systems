0cf74c1d2e1716a15905f17e9f8c1d3e9efaa02f
WoldemarK <kovtynov.vladimir@gmail.com>
2022-07-03 14:49:46 +0300

    add products you need to think through the logic

--- src/main/java/com/example/crmsystems/company/controller/ProductController.java
+++ src/main/java/com/example/crmsystems/company/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.example.crmsystems.company.controller;
 
+import com.example.crmsystems.company.model.Employee;
 import com.example.crmsystems.company.model.Product;
 import com.example.crmsystems.company.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -16,10 +20,55 @@ public class ProductController {
 
     @PostMapping("/")
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-//        Product prod = service.save(product);
-//        return product != null
-//                ? new ResponseEntity<>(prod, HttpStatus.CREATED)
-//                : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(service.save(product), HttpStatus.CREATED);
     }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Product>> getAllProduct() {
+        List<Product> empl = service.findAll();
+        return empl != null
+                ? new ResponseEntity<>(empl, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @GetMapping("/get/{productId}")
+    public ResponseEntity<Optional<Product>> getEmployeeById(@PathVariable Long productId) {
+        return Optional.ofNullable(service.findById(productId))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<Optional<Product>> getAllEmployeeParam(@RequestParam(required = false) Long productId) {
+        return Optional.ofNullable(service.findById(productId))
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> deleteById(@PathVariable Long productId) {
+        service.deleteById(productId);
+        return new ResponseEntity<>("Product deleted successfully!.", HttpStatus.OK);
+    }
+
+    @PutMapping("/emp/{productId}")
+    public ResponseEntity<Product> updateEmployee(@PathVariable(value = "productId") Long productId,
+                                                  @RequestBody Product prod) throws Exception {
+
+        Product product = service.findById(productId)
+                .orElseThrow(() -> new Exception("Employee not found for this id :: " + productId));
+
+        product.setCost(prod.getCost());
+        product.setType(prod.getType());
+        product.setModel(prod.getModel());
+        product.setBrand(product.getBrand());
+        product.setQuantity(prod.getQuantity());
+        product.setDescription(prod.getDescription());
+        Product updated = service.save(product);
+
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+
+    }
+
 }
--- src/main/java/com/example/crmsystems/company/service/ProductService.java
+++ src/main/java/com/example/crmsystems/company/service/ProductService.java
@@ -1,6 +1,5 @@
 package com.example.crmsystems.company.service;
 
-import com.example.crmsystems.company.model.Employee;
 import com.example.crmsystems.company.model.Product;
 import com.example.crmsystems.company.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,16 +18,16 @@ public class ProductService {
         return productsRepository.save(product);
     }
 
-//    public Optional<Product> findById(long id) {
-//        return productsRepository.findById(id);
-//    }
-//
-//    public List<Product> findAll() {
-//        return productsRepository.findAll();
-//    }
-//
-//    public void deleteById(long id) {
-//        productsRepository.deleteById(id);
-//    }
+    public Optional<Product> findById(long id) {
+        return productsRepository.findById(id);
+    }
+
+    public List<Product> findAll() {
+        return productsRepository.findAll();
+    }
+
+    public void deleteById(long id) {
+        productsRepository.deleteById(id);
+    }
 
 }
